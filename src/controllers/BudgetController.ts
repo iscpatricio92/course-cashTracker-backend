7468e63225287568c54731f349d645085ce35f21
PAt from Sit <isc.patricio@gmail.com>
2025-03-04 22:51:18 -0600

    feat: Implement budget creation and retrieval functionality

--- src/controllers/BudgetController.ts
+++ src/controllers/BudgetController.ts
@@ -1,13 +1,31 @@
 import type { Request, Response } from 'express';
+import Budget from '../models/Budget';
 export class BudgetController{
-    static getAll= (req: Request, res:Response) => {
-        res.send('Hello World')
+    static getAll= async(req: Request, res:Response, next:any) => {
+        try{
+            const budgets = await Budget.findAll({
+                order: [['createdAt', 'DESC']],
+                limit: 10,
+                //TODO: filter by user
+            })
+            res.status(201).json({data: budgets})
+        }
+        catch(error){
+            res.status(500).json({error: error.message})
+        }
     }
-    static create= (req: Request, res:Response) => {
-        res.send('Hello World')
+    static create= async (req: Request, res:Response) => {
+        try{
+            const budget = new Budget(req.body)
+            await budget.save()
+            res.status(201).json({message: 'Budget created successfully'})
+        }
+        catch(error){
+            res.status(500).json({error: error.message})
+        }
     }
     static getOne= (req: Request, res:Response) => {
-        res.send('Hello World')
+        
     }
     static update= (req: Request, res:Response) => {
         res.send('Hello World')
--- src/routes/budgetRouter.ts
+++ src/routes/budgetRouter.ts
@@ -1,10 +1,20 @@
 import { Router } from "express";
 import { BudgetController } from "../controllers/BudgetController";
+import {body} from 'express-validator'
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router()
 
 router.get('/', BudgetController.getAll)
-router.post('/', BudgetController.create)
+router.post('/',
+    body('name').notEmpty().withMessage('Budget name is required'),
+    body('amount')
+        .notEmpty().withMessage('Budget amount is required')
+        .isNumeric().withMessage('amount must be a number')
+        .custom((value) => value > 0).withMessage('amount must be greater than 0'),
+    handleInputErrors,
+    BudgetController.create
+)
 router.get('/:id', BudgetController.getOne)
 router.patch('/:id', BudgetController.update)
 router.delete('/:id', BudgetController.delete)
