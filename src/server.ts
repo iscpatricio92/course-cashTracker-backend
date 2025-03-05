9785d759afe775920f34fee27c0e795017fb9148
PAt from Sit <isc.patricio@gmail.com>
2025-03-04 20:49:19 -0600

    chore: Add database configuration and dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,21 @@
 import express from 'express'
 import colors from 'colors'
 import morgan from 'morgan'
+import { db } from './config/db'
 
+const connectDB=async()=>{
+    try{
+        await db.authenticate()
+        db.sync()
+        console.log(colors.blue.bold('Connection has been established successfully.'))
+    }
+    catch(error){
+        console.log(colors.red.bold(`Error: ${error.message}`))
+        process.exit(1)
+    }
+}
+
+connectDB()
 const app = express()
 
 app.use(morgan('dev'))
