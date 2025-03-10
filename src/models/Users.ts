d3bcb155eafdae832a7d8258ad8901082f8b1f49
PAt from Sit <isc.patricio@gmail.com>
2025-03-10 09:34:42 -0600

    chore: Add User model for managing user data

---
+++ src/models/Users.ts
@@ -0,0 +1,37 @@
+import { AllowNull, Column, DataType, Default, Model, Table, Unique } from "sequelize-typescript";
+
+@Table({
+    tableName: 'users',
+})
+
+class User extends Model{
+    @AllowNull(false)
+    @Column({
+        type: DataType.STRING(100),
+    })
+    declare name: string;
+
+    @AllowNull(false)
+    @Unique(true)
+    @Column({
+        type: DataType.STRING(50),
+    })
+    declare email: string;
+
+    @AllowNull(false)
+    @Column({
+        type: DataType.STRING(60),
+    })
+    declare password: string;
+
+    @Column({
+        type: DataType.STRING(6),
+    })
+    declare token: string;
+
+    @Default(false)
+    @Column({
+        type: DataType.BOOLEAN,
+    })
+    declare confirmed: string;
+}
