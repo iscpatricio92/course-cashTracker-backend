9a6ffd3ae8558585bc4e66f293c2755fb379497f
PAt from Sit <isc.patricio@gmail.com>
2025-03-22 17:43:25 -0600

    chore: Add express-rate-limit for rate limiting requests

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -2,8 +2,8 @@ import { Router } from "express";
 import { AuthController } from "../controllers/AuthContoller";
 import { body } from "express-validator";
 import { handleInputErrors } from "../middleware/validation";
+import { limiter } from "../config/limiter";
 const router = Router()
-
 router.post('/create-account',
     body('email').isEmail().withMessage('Email is not valid'),
     body('password').isLength({ min: 4 }).withMessage('Password must be at least 4 characters long'),
@@ -12,6 +12,7 @@ router.post('/create-account',
     AuthController.createAccount)
 
 router.post('/confirm-account',
+    limiter,
     body('token')
         .notEmpty()
         .isLength({ min: 6, max: 6 })
