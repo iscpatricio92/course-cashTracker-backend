f353c8cb232fa7340cccd1e56e3d1b4b1f9b6319
PAt from Sit <isc.patricio@gmail.com>
2025-03-23 03:48:10 -0600

    chore: Add pretest script to clear data before running tests

--- src/tests/integration/app.test.ts
+++ src/tests/integration/app.test.ts
@@ -1,9 +1,9 @@
-import request  from 'supertest';
+import request from 'supertest';
 import server, { connectDB } from '../../server';
 import { db } from '../../config/db';
 import { AUTH, CREATE_ACCOUNT } from '../../constants/endpoints';
 import { AuthController } from '../../controllers/AuthController';
-describe('CashTracker Integrations Test', () =>{
+describe('CashTracker Integrations Test', () => {
     /* beforeAll(async () => {
         await connectDB();
     }); */
@@ -34,6 +34,61 @@ describe('CashTracker Integrations Test', () =>{
                 expect(response.status).not.toBe(201);
                 expect(response.body.errors).not.toHaveLength(2);
                 expect(createAccountMock).not.toHaveBeenCalled();
+            });
+
+            it('should return 400 when email is invalid', async () => {
+                const response = await request(server)
+                    .post(`${AUTH}${CREATE_ACCOUNT}`)
+                    .send({
+                        "name": "John Doe",
+                        "email": "not_valid_email",
+                        "password": "123456"
+                    });
+
+                const createAccountMock = jest.spyOn(AuthController, 'createAccount');
+                expect(response.status).toBe(400);
+                expect(response.body).toHaveProperty('errors');
+                expect(response.body.errors).toHaveLength(1);
+                expect(response.body.errors[0].msg).toBe('Email is not valid');
+
+                expect(response.status).not.toBe(201);
+                expect(response.body.errors).not.toHaveLength(2);
+                expect(createAccountMock).not.toHaveBeenCalled();
+            });
+
+            it('should return 400 status code when password is less than 4 characters', async () => {
+                const response = await request(server)
+                    .post(`${AUTH}${CREATE_ACCOUNT}`)
+                    .send({
+                        "name": "John Doe",
+                        "email": "test@test.com",
+                        "password": "123"
+                    });
+
+                const createAccountMock = jest.spyOn(AuthController, 'createAccount');
+                expect(response.status).toBe(400);
+                expect(response.body).toHaveProperty('errors');
+                expect(response.body.errors).toHaveLength(1);
+                expect(response.body.errors[0].msg).toBe('Password must be at least 4 characters long');
+
+                expect(response.status).not.toBe(201);
+                expect(response.body.errors).not.toHaveLength(2);
+                expect(createAccountMock).not.toHaveBeenCalled();
+            });
+
+            it('should register a new user successfully', async () => {
+                const userData = {
+                    "name": "John Doe",
+                    "email": "test@test.com",
+                    "password": "123456"
+                };
+                const response = await request(server)
+                    .post(`${AUTH}${CREATE_ACCOUNT}`)
+                    .send(userData);
+
+                expect(response.status).toBe(201);
+                expect(response.status).not.toBe(400);
+                expect(response.body).toEqual({ message: "Account created successfully" });
             });
         });
     });
